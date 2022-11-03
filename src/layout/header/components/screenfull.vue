385c5f9c1a4a209a66e07d5f1d8809ca21545833
liuyx <1517482303@qq.com>
2022-11-03 16:54:51 +0800

    feat: 全屏显示

---
+++ src/layout/header/components/screenfull.vue
@@ -0,0 +1,18 @@
+<template>
+  <div @click="handleToggle">
+    <el-icon><Rank /></el-icon>
+  </div>
+</template>
+
+<script setup>
+import screenfull from 'screenfull'
+
+/**
+ * 利用screenfull插件来实现全屏切换
+ */
+const handleToggle = () => {
+  if (screenfull.isEnabled) {
+    screenfull.toggle()
+  }
+}
+</script>
